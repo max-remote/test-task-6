685336fc2b1bba25f7cb782d54f789bba639502e
Maxim Meshcheryakov <25-nv-86@mail.ru>
2022-11-24 01:23:55 +0600

    realized save data after change it in detail fragment and send it to list fragment on item

--- app/src/main/java/com/maxmesh/maxtask6/ui/fragments/DetailsContactFragment.kt
+++ app/src/main/java/com/maxmesh/maxtask6/ui/fragments/DetailsContactFragment.kt
@@ -6,15 +6,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import coil.load
+import com.maxmesh.maxtask6.R
 import com.maxmesh.maxtask6.databinding.FragmentContactDetailsBinding
 import com.maxmesh.maxtask6.domain.ContactEntity
+import com.maxmesh.maxtask6.ui.activity.MainActivity
 
 class DetailsContactFragment : Fragment() {
 
     private var _binding: FragmentContactDetailsBinding? = null
     private val binding get() = _binding!!
     private var contactEntity: ContactEntity? = null
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,16 +28,46 @@ class DetailsContactFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         arguments?.let { contactEntity = it.getParcelable(SAVE_CONTACT) }
-        setDataOnView()
+        setDataOnViewFromList()
+        clickOnSaveBtn()
     }
 
-    private fun setDataOnView() {
+    private fun setDataOnViewFromList() {
         with(binding) {
             editName.setText(contactEntity?.firstName)
             editLastName.setText(contactEntity?.surname)
             editPhone.setText(contactEntity?.phoneNumber)
             imageDetails.load(contactEntity?.avatarUrl)
         }
+    }
+
+    private fun clickOnSaveBtn() = with(binding) {
+        btnSave.setOnClickListener {
+            replaceContainer()
+            changeData()
+        }
+    }
+
+    private fun replaceContainer() {
+        requireActivity().supportFragmentManager.popBackStack()
+        requireActivity().supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.container, ListContactsFragment())
+            .commit()
+    }
+
+    private fun FragmentContactDetailsBinding.changeData() {
+        (requireActivity() as MainActivity)
+            .contacts[(requireActivity() as MainActivity).contacts.indexOf(
+            contactEntity
+        )] =
+            ContactEntity(
+                contactEntity?.id!!,
+                contactEntity?.avatarUrl!!,
+                editName.text.toString(),
+                editLastName.text.toString(),
+                editPhone.text.toString()
+            )
     }
 
     companion object {
